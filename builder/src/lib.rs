33ce22279faf738dec80365c15a276dc2926787f
emostov <32168567+emostov@users.noreply.github.com>
2021-03-29 15:17:34 -0700

    01-parse: passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,27 @@
-use proc_macro::TokenStream;
+use proc_macro::{Ident, TokenStream};
+use quote::{quote};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input  as DeriveInput);
 
-    unimplemented!()
+	let ident = ast.ident;
+	let builder_name = format!("{}Builder", stringify!(name));
+	let builder_ident = Ident::new(build_name);
+
+    let expanded = quote! {
+		pub struct #builder_ident {
+			executable: Option<String>,
+			args: Option<Vec<String>>,
+			env: Option<Vec<String>>,
+			current_dir: Option<String>,
+		}
+
+		impl #name {
+			pub fn builder() {}
+		}
+	 };
+
+	expanded.into()
 }
